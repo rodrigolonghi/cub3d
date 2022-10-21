c3f3d7e3b68707e58bfe6a2d91a770c6cf176203
Rodrigo Longhi <rodrigo.longhi@mercadolivre.com>
2022-10-20 21:21:15 -0300

    update collision

--- src/player.c
+++ src/player.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   player.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acarneir <acarneir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: rfelipe- <rfelipe-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 19:41:36 by rfelipe-          #+#    #+#             */
-/*   Updated: 2022/10/19 22:37:53 by acarneir         ###   ########.fr       */
+/*   Updated: 2022/10/20 21:17:41 by rfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,30 +28,64 @@ static void	y_walk(t_game *game)
 			game->movement_speed * game->y_walk);
 }
 
+static t_vec	check_square_collision(
+	t_game *game, t_vec new_pos, t_vec map_pos)
+{
+	t_vec	result;
+
+	map_pos.x /= 1.0;
+	map_pos.y /= 1.0;
+	result = create_vector(0, 0);
+	if (game->map.coordinates[(int)(map_pos.x)][(int)(map_pos.y)] > 0)
+	{
+		if ((new_pos.x + game->dist_wall < map_pos.x)
+			|| (new_pos.x - game->dist_wall > map_pos.x + 1)
+			|| (game->player_pos.y + game->dist_wall < map_pos.y)
+			|| (game->player_pos.y - game->dist_wall > map_pos.y + 1))
+			result.x = 1;
+		if ((new_pos.y + game->dist_wall < map_pos.y)
+			|| (new_pos.y - game->dist_wall > map_pos.y + 1)
+			|| (game->player_pos.x + game->dist_wall < map_pos.x)
+			|| (game->player_pos.x - game->dist_wall > map_pos.x + 1))
+			result.y = 1;
+		return (result);
+	}
+	return (create_vector(1, 1));
+}
+
 static t_vec	get_collision_vec(t_game *game, t_vec movement)
 {
-	t_vec	new_pos;
 	t_vec	collision_vec;
+	t_vec	new_pos;
 
+	collision_vec = create_vector(1, 1);
 	new_pos = vector_add(game->player_pos, movement);
-	// printf("player_pos = [%f, %f]\n", game->player_pos.x, game->player_pos.y);
-	// printf("new_pos1 = [%f, %f]\n", new_pos.x, new_pos.y);
-	collision_vec = create_vector(1.0, 1.0);
 	if (movement.x > 0.0 || movement.y > 0.0)
 	{
-		// printf("rows = %d, col = %d\n", game->map.rows, game->map.cols);
-		// printf("new_pox.x = %f\n", (new_pos.x + game->dist_wall));
-		// printf("new_pox.y = %f\n", (new_pos.y + game->dist_wall));
-		if (((int)(new_pos.x + game->dist_wall) >= game->map.rows))
-			collision_vec.x = 0.0;
-		else if (game->map.coordinates[(int)(new_pos.x + game->dist_wall)]
-				[(int)game->player_pos.y] != '0')
-			collision_vec.x = 0.0;
-		if ((int)(new_pos.y + game->dist_wall) >= game->map.cols)
-			collision_vec.y = 0.0;
-		else if (game->map.coordinates[(int)game->player_pos.x]
-			[(int)(new_pos.y + game->dist_wall)] != '0')
-			collision_vec.y = 0.0;
+		collision_vec = vector_mul(collision_vec, check_square_collision(game,
+					new_pos, create_vector(game->player_pos.x - 1,
+						game->player_pos.y - 1)));
+		collision_vec = vector_mul(collision_vec, check_square_collision(game,
+					new_pos, create_vector(game->player_pos.x - 1,
+						game->player_pos.y)));
+		collision_vec = vector_mul(collision_vec, check_square_collision(game,
+					new_pos, create_vector(game->player_pos.x - 1,
+						game->player_pos.y + 1)));
+		collision_vec = vector_mul(collision_vec, check_square_collision(game,
+					new_pos, create_vector(game->player_pos.x,
+						game->player_pos.y - 1)));
+		collision_vec = vector_mul(collision_vec, check_square_collision(game,
+					new_pos, create_vector(game->player_pos.x,
+						game->player_pos.y + 1)));
+		collision_vec = vector_mul(collision_vec, check_square_collision(game,
+					new_pos, create_vector(game->player_pos.x + 1,
+						game->player_pos.y - 1)));
+		collision_vec = vector_mul(collision_vec, check_square_collision(game,
+					new_pos, create_vector(game->player_pos.x + 1,
+						game->player_pos.y)));
+		collision_vec = vector_mul(collision_vec, check_square_collision(game,
+					new_pos, create_vector(game->player_pos.x + 1,
+						game->player_pos.y + 1)));
 	}
 	return (collision_vec);
 }
@@ -61,8 +95,6 @@ static void	update_input(t_game *game)
 	t_vec	movement;
 	t_vec	collision_vec;
 
-	// game->velocity = vector_mul_scal(game->velocity, game->movement_speed);
-	// game->strafe_vel = vector_mul_scal(game->strafe_vel, game->movement_speed);
 	movement = vector_add(game->velocity, game->strafe_vel);
 	collision_vec = get_collision_vec(game, movement);
 	movement = vector_mul(movement, collision_vec);
@@ -78,5 +110,5 @@ void	refresh_player(t_game *game)
 	x_walk(game);
 	y_walk(game);
 	update_input(game);
-	printf("player_pos = [%f, %f]\n", game->player_pos.x, game->player_pos.y);
+	// printf("player_pos = [%f, %f]\n", game->player_pos.x, game->player_pos.y);
 }
