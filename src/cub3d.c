016d832a6e58637d9277430afe5835572e58b308
a-cs <acarneirosousa@gmail.com>
2022-11-03 22:26:05 -0300

    removing temp vars

--- src/cub3d.c
+++ src/cub3d.c
@@ -6,23 +6,16 @@
 /*   By: acarneir <acarneir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 20:03:38 by rfelipe-          #+#    #+#             */
-/*   Updated: 2022/10/12 21:44:23 by acarneir         ###   ########.fr       */
+/*   Updated: 2022/11/03 22:24:41 by acarneir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-static void	temp_initiate_vars(t_game *game)
-{
-	game->ceilling = create_color(190, 190, 255);
-	game->floor = create_color(130, 130, 130);
-}
-
 int	main(int argc, char *argv[])
 {
 	t_game	game;
 
-	temp_initiate_vars(&game);
 	start_game(&game, argc, argv);
 	return (0);
 }
